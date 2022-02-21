aba3aedcdc2442e57ad11f0caf0fcb9c017f327c
maxjnorman <maxjnorman@gmail.com>
2022-02-21 20:07:01 +0000

    may as well set the logger level...

--- playground/app.R
+++ playground/app.R
@@ -1,9 +1,21 @@
 library(shiny)
 
+# logger threshold choices are...
+# logger::TRACE
+# logger::DEBUG
+# logger::INFO
+# ?? logger::SUCCESS ??
+# logger::WARN
+# logger::ERROR
+log_level <- logger::TRACE
+logger::log_threshold(log_level)
+logger::log_info("current log level is {log_level}:{attr(log_level, 'level')}")
+
+
 ui <- fluidPage()
 server <- function(input, output, session) {
   get_data <- reactive({
-    invalidateLater(millis = 2350)
+    invalidateLater(millis = 500)
     dat <- stringr::str_split(Sys.time(), " ")
     tbl <- tibble::tibble(x = dat[[1]][[1]], y = dat[[1]][[2]])
     return(tbl)
