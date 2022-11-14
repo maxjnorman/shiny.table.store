bec6ef17cb54880679988ec0571585c9a8d8bf60
Max Norman <maxjnorman@gmail.com>
2022-11-14 16:33:42 +0000

    more testing for fun_apply_schema

--- tests/testthat/test-utils-fun_apply_schema.R
+++ tests/testthat/test-utils-fun_apply_schema.R
@@ -1,6 +1,6 @@
 test_that("utils-fun_apply_schema-skip_keys_ignore", {
   data <- tibble::tibble("x" = as.character(1:3))
-  schema <- list("x" = as.character(9:1), "y" = as.character(9:1))
+  schema <- list("x" = as.character(9:1))
   out <- tibble::tibble("x" = factor(1:3, levels = 9:1))
   expect_out <- purrr::partial(expect_identical, expected = out)
   make_obj <- purrr::partial(fun_apply_schema, data = data, schema = schema)
@@ -13,11 +13,19 @@ test_that("utils-fun_apply_schema-skip_keys_ignore", {
   expect_out(make_obj(keys_ignore = c("y", "z")))
 })
 
-test_that("utils-fun_apply_schema-valid_keys_ignore", {
+test_that("utils-fun_apply_schema-full_keys_ignore", {
   data <- tibble::tibble("x" = as.character(1:3), "y" = as.character(4:6))
   schema <- list("x" = as.character(9:1), "y" = as.character(9:1))
   expect_data <- purrr::partial(expect_identical, expected = data)
   make_obj <- purrr::partial(fun_apply_schema, data = data, schema = schema)
   expect_data(make_obj(keys_ignore = c("x", "y")))
-  expect_data(make_obj(keys_ignore = c("x", "y", "z")))
+})
+
+test_that("utils-fun_apply_schema-valid_keys_ignore", {
+  data <- tibble::tibble("x" = as.character(1:3), "y" = as.character(4:6))
+  schema <- list("x" = as.character(9:1), "y" = as.character(9:1))
+  outX <- tibble::tibble("x" = factor(1:3, levels = 9:1), "y" = as.character(4:6))
+  outY <- tibble::tibble("y" = factor(4:6, levels = 9:1), "x" = as.character(1:3))
+  expect_identical(fun_apply_schema(data, schema, keys_ignore = c("y")), outX)
+  expect_identical(fun_apply_schema(data, schema, keys_ignore = c("x")), outY)
 })
