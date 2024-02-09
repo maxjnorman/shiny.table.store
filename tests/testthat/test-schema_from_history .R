042ce0478f3335c52998480756adc4cd780afb06
Max Norman <maxjnorman@gmail.com>
2024-02-09 11:08:00 +0000

    more tests

--- tests/testthat/test-schema_from_history .R
+++ tests/testthat/test-schema_from_history .R
@@ -1,3 +1,8 @@
-test_that("utils-schema_from_history", {
-  expect_mapequal("", schema_from_history(""))
+test_that("utils-schema_from_history-history_creation", {
+  expect_mapequal(list("x" = c("1", "2")), schema_from_history(list("00" = list("x" = "1"), "01" = list("x" = "2"))))
+  expect_mapequal(list("x" = c("2", "1")), schema_from_history(list("01" = list("x" = "1"), "00" = list("x" = "2"))))
+})
+test_that("utils-schema_from_history-data_types", {
+  expect_mapequal(list("x" = c("A", "2")), schema_from_history(list("00" = list("x" = "A"), "01" = list("x" = 2))))
+  expect_mapequal(list("x" = c("A", "2")), schema_from_history(list("00" = list("x" = "A"), "01" = list("x" = 2))))
 })
